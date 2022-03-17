4312822148f47d9bc65e1335b8bb63d6df4b9bc4
Akshayakumardas <94820847+Akshayakumardas@users.noreply.github.com>
2022-03-17 07:29:53 +0530

    Add files via upload

---
+++ dist/index.c36f364e.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('.body');
+const btn = document.querySelector('.btn');
+btn.addEventListener("click", function() {
+    console.log("javascript is running perfectly");
+});
+
+//# sourceMappingURL=index.c36f364e.js.map
